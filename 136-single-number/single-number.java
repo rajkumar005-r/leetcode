c19a16aaf9ca2138dbd76645b9b1a94f1bd65ce5
rajkumar005-r <rajkumarravichandhiran007@gmail.com>
2025-11-19 21:53:21 +0530

    Time: 10 ms (24.22%) | Memory: 47.3 MB (5.35%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        for(int i=1;i<n;i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                 return nums[i-1];
+            } 
+        }
+        return nums[nums.length-1];
+    }
+}
